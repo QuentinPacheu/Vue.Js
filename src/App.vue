3e94de8ec7c15595c175967e1cd985bc53c5c240
QuentinPacheu <quentin.pacheu@gmail.com>
2023-01-22 23:50:21 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link> |
+    <router-link to="/historique">Historique</router-link> |
+    <router-link to="/clavier">Clavier</router-link>
+    
   </nav>
   <router-view/>
 </template>
